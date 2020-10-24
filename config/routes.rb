eab5fcc8ce96c56f808d429cff7c21201014789a
daihi <theworldofimo@gmail.com>
2020-10-24 20:34:49 +0900

    コメント機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   devise_for :users
   root to: 'prototypes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :prototypes
+  resources :prototypes do
+    resources :comments, only: [:create]
+  end
 end
